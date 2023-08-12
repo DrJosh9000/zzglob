7662e67d3dfd2cfd9697684d1b049280fb284d30
Josh Deprez <josh.deprez@gmail.com>
2023-08-12 21:43:51 +1000

    WIP

--- expr.go
+++ expr.go
@@ -1,7 +1,7 @@
 package zzglob
 
 type expression interface {
-	match(rune) (matched, proceed bool)
+	match(rune) (match, split bool)
 }
 
 // Expressions
@@ -22,11 +22,11 @@ type (
 	questionExp struct{}
 )
 
-func (e literalExp) match(r rune) (bool, bool) { return rune(e) == r, true }
-func (pathSepExp) match(r rune) (bool, bool)   { return r == '/', true }
-func (starExp) match(r rune) (bool, bool)      { return r != '/', false }
-func (doubleStarExp) match(rune) (bool, bool)  { return true, false }
-func (questionExp) match(r rune) (bool, bool)  { return r != '/', true }
+func (e literalExp) match(r rune) (bool, bool) { return rune(e) == r, false }
+func (pathSepExp) match(r rune) (bool, bool)   { return r == '/', false }
+func (starExp) match(r rune) (bool, bool)      { return r != '/', true }
+func (doubleStarExp) match(rune) (bool, bool)  { return true, true }
+func (questionExp) match(r rune) (bool, bool)  { return r != '/', false }
 
 type edge struct {
 	Expr expression
@@ -35,4 +35,34 @@ type edge struct {
 
 type node struct {
 	Out []edge
+}
+
+func (n *node) terminal() bool { return len(n.Out) == 0 }
+
+func match(start *node, path string) bool {
+	nodes := []*node{start}
+	var next []*node
+	for _, r := range path {
+		if len(nodes) == 0 {
+			return false
+		}
+		for _, n := range nodes {
+			for _, e := range n.Out {
+				m, s := e.Expr.match(r)
+				if m {
+					next = append(next, e.Node)
+					if s {
+						next = append(next, n)
+					}
+				}
+			}
+		}
+		nodes, next = next, nodes[:0]
+	}
+	for _, n := range nodes {
+		if n.terminal() {
+			return true
+		}
+	}
+	return false
 }
---
+++ expr_test.go
@@ -0,0 +1,36 @@
+package zzglob
+
+import "testing"
+
+func TestMatch(t *testing.T) {
+	tests := []struct {
+		pattern, path string
+		want          bool
+	}{
+		{"a*b", "acccccb", true},
+		{"a*b", "abc", false},
+		{"a*b", "a/b", false},
+		{"a/{b,c}/d", "a/c/d", true},
+		{"a/{b,c}/d", "a/w/d", false},
+		{"a/[bc]/d", "a/b/d", true},
+		{"a/[bc]/d", "a/x/d", false},
+		{"a?b", "acb", true},
+		{"a?b", "accb", false},
+		{"a**b", "acb", true},
+		{"a**b", "acccb", true},
+		{"a**b", "a/b", true},
+		{"a**b", "a/c/b", true},
+		{"a**b", "a/b/c", false},
+	}
+
+	for _, test := range tests {
+		_, start, err := parse(test.pattern)
+		if err != nil {
+			t.Fatalf("parse(%q) error = %v", test.pattern, err)
+		}
+
+		if got, want := match(start, test.path), test.want; got != want {
+			t.Errorf("match(start, %q) = %v, want %v", test.path, got, want)
+		}
+	}
+}
