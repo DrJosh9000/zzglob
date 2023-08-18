cf91ba9da2eb882967473dbf8adbbe14629eb7b0
Josh Deprez <josh.deprez@gmail.com>
2023-08-18 17:25:03 +1000

    WIP

---
+++ state.go
@@ -0,0 +1,40 @@
+package zzglob
+
+type state struct {
+	Out []edge
+}
+
+func (s *state) terminal() bool { return len(s.Out) == 0 }
+
+func matchSegment(set []*state, segment string) []*state {
+	var next []*state
+	for _, r := range segment {
+		if len(set) == 0 {
+			return nil
+		}
+		for _, n := range set {
+			for _, e := range n.Out {
+				matched, keep := e.Expr.match(r)
+				if !matched {
+					continue
+				}
+				next = append(next, e.State)
+				if keep {
+					next = append(next, n)
+				}
+			}
+		}
+		set, next = next, set[:0]
+	}
+	return set
+}
+
+func match(start *state, path string) bool {
+	set := matchSegment([]*state{start}, path)
+	for _, n := range set {
+		if n.terminal() {
+			return true
+		}
+	}
+	return false
+}
