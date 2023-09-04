69fd6cfbd349931d9962cc66f13fd4b31391ad83
Josh Deprez <jd@buildkite.com>
2023-09-04 10:15:58 +1000

    Totally change Glob API

--- glob.go
+++ glob.go
@@ -2,6 +2,7 @@
 package zzglob
 
 import (
+	"errors"
 	"io/fs"
 	"os"
 	"path"
@@ -9,7 +10,11 @@ import (
 )
 
 // Glob globs for files matching the pattern in a filesystem.
-func (p *Pattern) Glob(f fs.WalkDirFunc, traverseSymlinks bool) error {
+func (p *Pattern) Glob(f fs.WalkDirFunc, opts ...GlobOption) error {
+	if f == nil {
+		return errors.New("nil WalkDirFunc in arg to Glob")
+	}
+
 	if p.initial == nil {
 		fi, err := os.Stat(p.root)
 		if err != nil {
@@ -18,110 +23,134 @@ func (p *Pattern) Glob(f fs.WalkDirFunc, traverseSymlinks bool) error {
 		return f(p.root, nil, nil)
 	}
 
-	// This is a queue of roots, and the partial matching states to get to that
-	// root.
-	type globState struct {
-		root   string
-		states map[*state]struct{}
+	gs := globState{
+		opts: &globConfig{
+			TraverseSymlinks: true,
+			Callback:         f,
+		},
+		root:   p.root,
+		states: singleton(p.initial),
 	}
-	queue := []globState{{p.root, singleton(p.initial)}}
 
-	for len(queue) > 0 {
-		gs := queue[0]
-		queue = queue[1:]
+	for _, o := range opts {
+		o(gs.opts)
+	}
 
-		//println("starting walk at", gs.root, "with", len(gs.states), "states")
+	//println("starting walk at", gs.root, "with", len(gs.states), "states")
+	return fs.WalkDir(os.DirFS(gs.root), ".", gs.walkDirFunc)
+}
 
-		if err := fs.WalkDir(os.DirFS(gs.root), ".", func(fp string, d fs.DirEntry, err error) error {
-			// Yes yes, very good.
-			if fp == "." {
-				return nil
-			}
+// GlobOption is the type for options that apply to Glob.
+type GlobOption = func(*globConfig)
 
-			// Directories have a trailing slash for matching.
-			// (Symlinks to other directories won't get a slash here.)
-			if d != nil && d.IsDir() && !strings.HasSuffix(fp, "/") {
-				fp += "/"
-			}
+type globConfig struct {
+	TraverseSymlinks bool
+	Callback         fs.WalkDirFunc
+}
 
-			// Match p against the state machine.
-			states := matchSegment(gs.states, fp)
+// TraverseSymlinks enables or disables the traversal of symlinks during
+// globbing. It is enabled by default.
+func TraverseSymlinks(traverse bool) GlobOption {
+	return func(opts *globConfig) {
+		opts.TraverseSymlinks = traverse
+	}
+}
 
-			// Some debugging code that might be handy:
-			// println("matchSegment(", len(gs.states), fp, ") ->", len(states), "states")
-			// if len(states) > 0 {
-			// 	p.WriteDot(os.Stderr, states)
-			// }
+type globState struct {
+	opts   *globConfig
+	root   string
+	states map[*state]struct{}
+}
 
-			// Join fp back onto root, for passing back to f and for other
-			// operations.
-			fp = path.Join(gs.root, fp)
+func (gs *globState) walkDirFunc(fp string, d fs.DirEntry, err error) error {
+	// Yes yes, very good.
+	if fp == "." {
+		return nil
+	}
 
-			// Did it match in any way?
-			if len(states) == 0 {
-				if d != nil && d.IsDir() {
-					// Skip - not interested in anything in this directory.
-					return fs.SkipDir
-				}
+	// Directories have a trailing slash for matching.
+	// (Symlinks to other directories won't get a slash here.)
+	if d != nil && d.IsDir() && !strings.HasSuffix(fp, "/") {
+		fp += "/"
+	}
 
-				// This non-directory thing doesn't match. Don't return
-				// fs.SkipDir, since that skips the remainder of the directory.
-				return nil
-			}
+	// Match p against the state machine.
+	states := matchSegment(gs.states, fp)
 
-			if err != nil {
-				// Report the error to the callback.
-				return f(fp, d, err)
-			}
+	// Some debugging code that might be handy:
+	// println("matchSegment(", len(gs.states), fp, ") ->", len(states), "states")
+	// if len(states) > 0 {
+	// 	p.WriteDot(os.Stderr, states)
+	// }
 
-			// So we matched, either partially or fully.
-			terminal := false
-			for s := range states {
-				if s.Terminal {
-					terminal = true
-					break
-				}
-			}
+	// Join fp back onto root, for passing back to f and for other
+	// operations.
+	fp = path.Join(gs.root, fp)
 
-			// Did the pattern match completely?
-			if terminal {
-				// Give it to the callback.
-				return f(fp, d, err)
-			}
+	// Did it match in any way?
+	if len(states) == 0 {
+		if d != nil && d.IsDir() {
+			// Skip - not interested in anything in this directory.
+			return fs.SkipDir
+		}
 
-			// The pattern matched only partially.
-			// Are we traversing symlinks? Is it a symlink?
-			if traverseSymlinks {
-				fi, err := os.Lstat(fp)
-				if err != nil {
-					// Can't lstat, can't tell if link.
-					return f(fp, fs.FileInfoToDirEntry(fi), err)
-				}
-				if fi.Mode()&os.ModeSymlink != os.ModeSymlink {
-					// Not a symlink.
-					return nil
-				}
+		// This non-directory thing doesn't match. Don't return
+		// fs.SkipDir, since that skips the remainder of the directory.
+		return nil
+	}
 
-				// Make sure we're matching a directory here...
-				states = matchSegment(states, "/")
-				if len(states) == 0 {
-					return nil
-				}
+	if err != nil {
+		// Report the error to the callback.
+		return gs.opts.Callback(fp, d, err)
+	}
 
-				// Walk the symlink by enqueueing a new root.
-				// fs.WalkDir doesn't walk symlinks unless it is the root path!
-				queue = append(queue, globState{
-					root:   fp,
-					states: states,
-				})
-			}
-
-			// Continue walking as normal.
-			return nil
-
-		}); err != nil {
-			return err
+	// So we matched, either partially or fully.
+	terminal := false
+	for s := range states {
+		if s.Terminal {
+			terminal = true
+			break
 		}
 	}
-	return nil
+
+	// Did the pattern match completely?
+	if terminal {
+		// Give it to the callback.
+		return gs.opts.Callback(fp, d, err)
+	}
+
+	// The pattern matched only partially.
+	// Are we traversing symlinks? Is it a symlink?
+	if !gs.opts.TraverseSymlinks {
+		return nil
+	}
+
+	// It's all symlink from this point.
+
+	fi, err := os.Lstat(fp)
+	if err != nil {
+		// Can't lstat, can't tell if link.
+		return gs.opts.Callback(fp, fs.FileInfoToDirEntry(fi), err)
+	}
+	if fi.Mode()&os.ModeSymlink != os.ModeSymlink {
+		// Not a symlink.
+		return nil
+	}
+
+	// Make sure we're matching a directory here...
+	states = matchSegment(states, "/")
+	if len(states) == 0 {
+		return nil
+	}
+
+	// Walk the symlink by... recursion.
+	// fs.WalkDir doesn't walk symlinks unless it is the root path... in
+	// which case it does!
+	next := globState{
+		opts:   gs.opts,
+		root:   fp,
+		states: states,
+	}
+	//println("starting walk at", next.root, "with", len(next.states), "states")
+	return fs.WalkDir(os.DirFS(next.root), ".", next.walkDirFunc)
 }
--- glob_test.go
+++ glob_test.go
@@ -23,7 +23,35 @@ func TestGlob(t *testing.T) {
 	if err := p.Glob(func(path string, d fs.DirEntry, err error) error {
 		got = append(got, walkFuncArgs{path, err})
 		return nil
-	}, true); err != nil {
+	}); err != nil {
+		t.Fatalf("Glob(...) = %v", err)
+	}
+
+	want := []walkFuncArgs{
+		{Path: "fixtures/a/b/cd/elf/g/j/absurdity/m"},
+		{Path: "fixtures/a/b/cid/erf/h/k/m"},
+		{Path: "fixtures/a/b/cid/erf/h/k/n/m"},
+		{Path: "fixtures/a/b/cod/erf/h/k/m"},
+		{Path: "fixtures/a/b/cod/erf/h/k/n/m"},
+	}
+
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("walked paths diff (-got +want):\n%s", diff)
+	}
+}
+
+func TestGlob_TraverseSymlinksDisabled(t *testing.T) {
+	pattern := "fixtures/a/b/c*d/e?f/[ghi]/{j,k,l}/**/m"
+	p, err := Parse(pattern)
+	if err != nil {
+		t.Fatalf("Parse(%q) = %v", pattern, err)
+	}
+
+	var got []walkFuncArgs
+	if err := p.Glob(func(path string, d fs.DirEntry, err error) error {
+		got = append(got, walkFuncArgs{path, err})
+		return nil
+	}, TraverseSymlinks(false)); err != nil {
 		t.Fatalf("Glob(...) = %v", err)
 	}
 
@@ -31,8 +59,6 @@ func TestGlob(t *testing.T) {
 		{Path: "fixtures/a/b/cd/elf/g/j/absurdity/m"},
 		{Path: "fixtures/a/b/cod/erf/h/k/m"},
 		{Path: "fixtures/a/b/cod/erf/h/k/n/m"},
-		{Path: "fixtures/a/b/cid/erf/h/k/m"},
-		{Path: "fixtures/a/b/cid/erf/h/k/n/m"},
 	}
 
 	if diff := cmp.Diff(got, want); diff != "" {
